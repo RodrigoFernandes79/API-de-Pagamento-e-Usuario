6f4f6405690c8d8351fbcf18e0974d1ad1fe95a8
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-06-25 13:13:19 -0300

    Criando classes Domain,interface Repository de User, e commandLineRunner

--- src/main/java/com/userapi/UserApiApplication.java
+++ src/main/java/com/userapi/UserApiApplication.java
@@ -1,13 +1,31 @@
 package com.userapi;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.userapi.domain.User;
+import com.userapi.repositories.UserRepository;
+
 @SpringBootApplication
-public class UserApiApplication {
+public class UserApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.saveAll(List.of(
+				new User(null, "Rodrigo Fernandes","123","rodrigo@email",21.00)
+				));
+		
 	}
 
 }
